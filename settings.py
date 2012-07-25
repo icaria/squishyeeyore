954ee991b9dc4b580a1d643ea6e4429bbebf6920
Stephen Chen <stephenchen0@gmail.com>
2012-07-25 10:31:28 -0400

    Dynamic template path generated.

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for schoolime project.
+import os, sys
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -106,6 +107,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    
+    os.path.join(os.getcwd(), "template"),
 )
 
 INSTALLED_APPS = (
