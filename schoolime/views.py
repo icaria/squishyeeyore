ce0ee629ea3b7b1a4cc076ac62bac436954beea7
Stephen Chen <stephenchen0@gmail.com>
2012-07-25 12:19:55 -0400

    Hmmm

--- schoolime/views.py
+++ schoolime/views.py
@@ -7,18 +7,14 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def home_page(request):
-    """
-    If users are authenticated, direct them to the main page. Otherwise, take
-    them to the login page.
-    """
+    #If users are authenticated, direct them to the main page. Otherwise, take
+    #them to the login page.
     return render_to_response('home.html')
 
 def main_page(request):
     return render_to_response('index.html')
 
 def logout_page(request):
-    """
-    Log users out and re-direct them to the main page.
-    """
+    #Log users out and re-direct them to the main page.
     logout(request)
     return HttpResponseRedirect('/')
