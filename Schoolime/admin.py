1cb6177f17cd9331345fa61525c5fdb7d0929ad6
Stephen Chen <stephenchen0@gmail.com>
2012-07-25 10:58:49 -0400

    Mmmm.

--- Schoolime/admin.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Jul 25, 2012
-
-@author: Stephen Chen
-'''
-#from django.contrib import admin
-#from schoolime.models import Schoolime
-
-#admin.site.register(Schoolime)
--- Schoolime/views.py
+++
@@ -1,4 +0,0 @@
-# Create your views here.
-# Define views with templates in order to produce dynamic contented pages.
-
-class 
